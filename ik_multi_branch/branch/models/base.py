4bc5065b3770436785da6cb4438f3745c62e1479
madux3 <kingstongerald@gmail.com>
2026-06-01 12:34:50 +0100

    [FIX]

--- ik_multi_branch/branch/models/base.py
+++ ik_multi_branch/branch/models/base.py
@@ -18,13 +18,13 @@ class ResPartner(models.Model):
         return self.env.user.branch_id
 
     branch_id = fields.Many2one('multi.branch', string='Branch', default=_branch_default_get)
-    hp_number = fields.Char(string='HP nummber')
+    # hp_number = fields.Char(string='HP nummber')
 
-    @api.constrains('hp_number')
-    def check_hp_number_constraint(self):
-        exist_hp_number = self.env['res.partner'].search([
-            ('hp_number', '=', self.hp_number)], limit=2)
-        if len(exist_hp_number) > 1:
-            raise ValidationError("Sorry !!! you cannot create record with duplicate hp_number")
+    # @api.constrains('hp_number')
+    # def check_hp_number_constraint(self):
+    #     exist_hp_number = self.env['res.partner'].search([
+    #         ('hp_number', '=', self.hp_number)], limit=2)
+    #     if len(exist_hp_number) > 1:
+    #         raise ValidationError("Sorry !!! you cannot create record with duplicate hp_number")
       
 
